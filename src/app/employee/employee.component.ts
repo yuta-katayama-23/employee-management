1aae38ce3ebb15efcb6060b2beeb9cf35a066db2
Yuta Katayama <yutapilafu@gmail.com>
2021-04-06 20:11:32 +0900

    社員一覧画面の修正

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 import { MessageService } from '../message.service';
@@ -11,11 +12,11 @@ import { MessageService } from '../message.service';
 export class EmployeeComponent implements OnInit {
 
   employees: Employee[];
-  selectedEmployee: Employee;
 
   constructor(
     private employeeService: EmployeeService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -23,12 +24,12 @@ export class EmployeeComponent implements OnInit {
   }
 
   onSelect(employee: Employee): void {
-    this.selectedEmployee = employee;
     this.messageService.add(`EmployeeComponent: 社員データ(id=${employee.id})が選択されました`);
+    this.router.navigate([`/detail/${employee.id}`]);
   }
 
   getEmployee(): void {
-    this.employeeService.getEmployee().subscribe(employees => this.employees = employees);
+    this.employeeService.getEmployees().subscribe(employees => this.employees = employees);
   }
 
 }
